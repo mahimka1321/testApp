99033923db8b3a8b1930ed95ad25537274949d43
mahimka1321 <xakepok7771@mail.ru>
2023-07-23 18:33:21 +0300

    com 9

--- src/InfoTour.jsx
+++ src/InfoTour.jsx
@@ -33,6 +33,9 @@ function InfoTour() {
                         <div className='status__tour'>
                             <p className='star_'>
                                 <span>
+                                    <svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M6.99998 0L9.16285 4.8856L14 5.66901L10.5 9.47191L11.326 14.8418L6.99998 12.3065L2.67372 14.8418L3.50001 9.47191L0 5.66901L4.83685 4.8856L6.99998 0Z" fill="#FAC917"/>
+                                    </svg>
                                 </span>
                                 4.9
                             </p>
