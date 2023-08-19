15ba5e179374be66d85aed7ec14c95caac708c8b
mahimka1321 <xakepok7771@mail.ru>
2023-08-19 17:02:38 +0300

    com fix 54.6

--- src/NotPoage.jsx
+++ src/NotPoage.jsx
@@ -2,15 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-function NotPoage(props) {
+function NotPoage() {
 
     
     return (
-        <>
-            <h1>Страница не найдена или была неправильно загруженна</h1>
-            <p>Найдите страницу в ручную</p>
-            <Link to='/'>Вернуться на главную страницу</Link>
-        </>
+        <div style={{
+
+            display: 'flex',
+            width: '100%',
+            height: '100svh',
+            justifyContent: 'center',
+            alignItems: 'center'
+        
+        }}>
+            <div style={{color:'#616161', marginLeft:'20px'}}>
+                <h1>Страница не найдена или была неправильно загруженна</h1>
+                <p>Найдите страницу в ручную</p>
+                <Link style={{textDecoration:'none'}} to='/'>Вернуться на главную страницу</Link>
+            </div>
+        </div>
     );
 }
 
